515d38261d328ee309d3643677f4e63a46468eaa
DESKTOP-49G2K12\Sanion <jlee431@sanion.com>
2021-09-06 09:49:34 +0900

    First Commit

---
+++ MyMobileTrackerList/Models/MyMobileTracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMobileTrackerList.Models
+{
+    public class MyMobileTracker
+    {
+        public int Id { get; set; }
+        public int HitCount { get; set; }
+        public DateTime CurrentTime { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
---
+++ MyMobileTrackerList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMobileTrackerList.Startup))]
+namespace MyMobileTrackerList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
